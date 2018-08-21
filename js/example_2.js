934e9c5804c73fc3315d28dd9bef8fd65a3a8aee
Christian Grimberg <christian.grimberg@outlook.com>
2018-08-20 21:25:48 -0300

    :monkey_face: Dibujando lineas con Canvas mediante el uso de funciones en JavaScript.

--- js/example_2.js
+++ js/example_2.js
@@ -1,21 +1,22 @@
 var element2;
 var draw;
 
+function drawLine(color, x_initial, y_initial, x_final, y_final)
+{
+    draw.beginPath();
+    draw.strokeStyle = color;
+    draw.moveTo(x_initial, y_initial);
+    draw.lineTo(x_final, y_final);
+    draw.stroke();
+    draw.closePath();   
+}
+
 element2 = document.getElementById("example_2");
 draw = element2.getContext("2d");
 
 console.log(draw);
 
-draw.beginPath();
-draw.strokeStyle = "red";
-draw.moveTo(100, 100);
-draw.lineTo(200, 200);
-draw.stroke();
-draw.closePath();
-
-draw.beginPath();
-draw.strokeStyle = " blue";
-draw.moveTo(200, 100);
-draw.lineTo(100, 200);
-draw.stroke();
-draw.closePath();
+drawLine("red", 100, 100, 200, 200);
+drawLine("blue", 200, 100, 100, 200);
+drawLine("yellow", 100, 150, 200, 150);
+drawLine("pink", 150, 100, 150, 200);
