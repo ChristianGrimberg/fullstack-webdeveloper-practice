190c7256e673e737c08018e38bd5973b7b25a74b
Christian Grimberg <christian.grimberg@outlook.com>
2018-08-24 20:21:48 -0300

    :+1: Se borra el canvas antes de generar las lineas en cada click.

--- js/example_2.js
+++ js/example_2.js
@@ -1,13 +1,17 @@
-var element2;
-var draw;
-var width;
-drawingLines = document.getElementById("drawingLines");
-txtQtyLines = document.getElementById("txtQtyLines");
-btnSendQtyLines = document.getElementById("btnSendQtyLines");
-
-drawContext = drawingLines.getContext("2d");
-width = drawingLines.width;
+var lines;
+var space;
+var txtQtyLines = document.getElementById("txtQtyLines");
+var drawingLines = document.getElementById("drawingLines");
+var drawContext = drawingLines.getContext("2d");
+var width = drawingLines.width;
+var height = drawingLines.height;
+var btnSendQtyLines = document.getElementById("btnSendQtyLines");
 btnSendQtyLines.addEventListener("click", drawLinesAfterClick);
+
+function clearCanvas()
+{
+    drawContext.clearRect(0, 0, width, height);
+}
 
 function drawLine(color, x_initial, y_initial, x_final, y_final)
 {
@@ -20,19 +24,24 @@ function drawLine(color, x_initial, y_initial, x_final, y_final)
 }
 
 function drawLinesAfterClick()
-{    
-    var lines = parseInt(txtQtyLines.value);
+{
     var yi, xf;
     var color = "#FAA";
-    var space = width / lines;
 
-    for(var i = 0; i<lines; i++)
+    clearCanvas();
+    lines = parseInt(txtQtyLines.value);
+    space = width/lines;   
+
+    if(lines<width)
     {
-        yi = space * i;
-        xf = space * (i + 1);
-        drawLine(color, 0, yi, xf, width);
-    }
+        for(var i = 0; i<lines; i++)
+        {
+            yi = space * i;
+            xf = space * (i + 1);
+            drawLine(color, 0, yi, xf, width);
+        }
+    }    
 
-    drawLine(color, 1, 1, 1, width-1);
+    drawLine(color, 1, 1, 1, height-1);
     drawLine(color, 1, width-1, width-1, width-1);
 }
