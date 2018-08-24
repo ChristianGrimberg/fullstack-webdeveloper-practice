dacb9dfe3838d1aef04563f3b80e3f154e2068bf
Christian Grimberg <christian.grimberg@outlook.com>
2018-08-24 13:01:30 -0300

    :+1: Se invoca la funcion de dibujado de lineas en el evento de click de boton.

--- js/example_2.js
+++ js/example_2.js
@@ -3,7 +3,9 @@ var draw;
 drawingLines = document.getElementById("drawingLines");
 txtQtyLines = document.getElementById("txtQtyLines");
 btnSendQtyLines = document.getElementById("btnSendQtyLines");
+
 drawContext = drawingLines.getContext("2d");
+btnSendQtyLines.addEventListener("click", drawLineAfterClick);
 
 function drawLine(color, x_initial, y_initial, x_final, y_final)
 {
@@ -15,10 +17,11 @@ function drawLine(color, x_initial, y_initial, x_final, y_final)
     drawContext.closePath();   
 }
 
-//console.log(drawContext);
-
-/**< Dibujo de lineas en canvas con el id correspondiente */
-drawLine("red", 100, 100, 200, 200);
-drawLine("blue", 200, 100, 100, 200);
-drawLine("yellow", 100, 150, 200, 150);
-drawLine("pink", 150, 100, 150, 200);
+function drawLineAfterClick()
+{    
+    /**< Dibujo de lineas en canvas con el id correspondiente */
+    drawLine("red", 100, 100, 200, 200);
+    drawLine("blue", 200, 100, 100, 200);
+    drawLine("yellow", 100, 150, 200, 150);
+    drawLine("pink", 150, 100, 150, 200);
+}
